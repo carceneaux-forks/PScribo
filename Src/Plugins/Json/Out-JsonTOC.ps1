585a413aad272bdb867cb9d66076b0980d9e8ebb
Chris Arceneaux <carcenea@gmail.com>
2022-12-01 16:43:47 -0500

    greatly simplified
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTOC.ps1
+++ Src/Plugins/Json/Out-JsonTOC.ps1
@@ -18,32 +18,17 @@ function Out-JsonTOC {
     process {
         $tocBuilder = New-Object -TypeName 'System.Collections.ArrayList'
 
-        if ($Options.ContainsKey('EnableSectionNumbering')) {
-            $maxSectionNumberLength = $Document.TOC.Number | ForEach-Object { $_.Length } | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum
+        if ($Options.ContainsKey('EnableSectionNumbering')) {            
             foreach ($tocEntry in $Document.TOC) {
-                $sectionNumberPaddingLength = $maxSectionNumberLength - $tocEntry.Number.Length
-                $sectionNumberIndent = ''.PadRight($tocEntry.Level, ' ')
-                $sectionPadding = ''.PadRight($sectionNumberPaddingLength, ' ')
-                [ref] $null = $tocBuilder.AppendFormat('"{0}{1}  {2}{3}"', $tocEntry.Number, $sectionPadding, $sectionNumberIndent, $tocEntry.Name)
+                [ref] $null = $tocBuilder.Add(@{"Section"=$tocEntry.Number;"Name"=$tocEntry.Name})
             }
         }
         else {
-            Write-Host $($Document.TOC.Number | ForEach-Object { $_.Length } | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum)
             foreach ($tocEntry in $Document.TOC) {
-                Write-Host "BEGIN"
-                [Version]$level = "$($tocEntry.Number).$($tocEntry.Level)"
-                Write-Host $level
-                Write-Host "END"
-                # if ($tocEntry.Level -gt ($tocBuilder.Count-1)) {
-                #     $tocBuilder[$tocEntry.Name] = @{}
-                #     [ref] $null = $list.Add($tocEntry.Name)
-                #     [ref] $null = $tocBuilder.Add($list)
-                #     [ref] $null = $list
-                # }
-                # [ref] $null = $tocBuilder[$tocEntry.Level].Add($tocEntry.Name)
+                [ref] $null = $tocBuilder.Add(@{"Name"=$tocEntry.Name})
             }
         }
         Write-Host ($tocBuilder | ConvertTo-Json)
-        return ($tocBuilder | ConvertTo-Json)
+        return ($tocBuilder)
     }
 }
