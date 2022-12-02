8454dd7b18e743e4b08b026c6cb6323808de3e56
Chris Arceneaux <carcenea@gmail.com>
2022-12-02 09:21:27 -0500

    test
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTOC.ps1
+++ Src/Plugins/Json/Out-JsonTOC.ps1
@@ -32,20 +32,20 @@ function Out-JsonTOC {
                     }
                     1 {
                         $level1 = $tocBuilder.Count - 1
-                        [ref] $null = $tocBuilder[$level1].Add($tocEntry.Number,@{"Section"=$tocEntry.Number;"Name"=$tocEntry.Name})
+                        [ref] $null = $tocBuilder[$level1].Add($tocEntry.Number,[ordered]@{"Section"=$tocEntry.Number;"Name"=$tocEntry.Name})
                         break
                     }
                     2 {
                         $level1 = $tocBuilder.Count - 1
                         $level2 = $tocBuilder[$level1].Count - 1
-                        [ref] $null = $tocBuilder[$level1][$level2].Add($tocEntry.Number,@{"Section"=$tocEntry.Number;"Name"=$tocEntry.Name})
+                        [ref] $null = $tocBuilder[$level1][$level2].Add($tocEntry.Number,[ordered]@{"Section"=$tocEntry.Number;"Name"=$tocEntry.Name})
                         break
                     }
                     3 {
                         $level1 = $tocBuilder.Count - 1
                         $level2 = $tocBuilder[$level1].Count - 1
                         $level3 = $tocBuilder[$level1][$level2].Count - 1
-                        [ref] $null = $tocBuilder[$level1][$level2][$level3].Add($tocEntry.Number,@{"Section"=$tocEntry.Number;"Name"=$tocEntry.Name})
+                        [ref] $null = $tocBuilder[$level1][$level2][$level3].Add($tocEntry.Number,[ordered]@{"Section"=$tocEntry.Number;"Name"=$tocEntry.Name})
                         break
                     }
                 }                
