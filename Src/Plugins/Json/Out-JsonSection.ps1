2035597a59b18c514c369cb715186703e3ea33da
Chris Arceneaux <carcenea@gmail.com>
2022-12-02 15:39:16 -0500

    Initial JSON support
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonSection.ps1
+++ Src/Plugins/Json/Out-JsonSection.ps1
@@ -13,26 +13,22 @@ function Out-JsonSection
     )
     begin
     {
-        ## Fix Set-StrictMode
-        if (-not (Test-Path -Path Variable:\Options))
-        {
-            $options = New-PScriboJsonOption
-        }
+        ## Initializing section object
+        $sectionBuilder = [ordered]@{}
     }
     process
     {
-        $sectionBuilder = New-Object -TypeName System.Text.StringBuilder
-        if ($Document.Options['EnableSectionNumbering'])
-        {
+        # if ($Document.Options['EnableSectionNumbering'])
+        # {
             [string] $sectionName = '{0} {1}' -f $Section.Number, $Section.Name
-        }
-        else
-        {
-            [string] $sectionName = '{0}' -f $Section.Name
-        }
-        [ref] $null = $sectionBuilder.AppendFormat('"{0}": [',$sectionName.TrimStart()).AppendLine()
+        # }
+        # else
+        # {
+        #     [string] $sectionName = '{0}' -f $Section.Name
+        # }
+        # [ref] $null = $sectionBuilder.AppendFormat('"{0}": [',$sectionName.TrimStart()).AppendLine()
 
-        foreach ($subSection in $Section.Sections.GetEnumerator())
+        $section = foreach ($subSection in $Section.Sections.GetEnumerator())
         {
             $currentIndentationLevel = 1
             if ($null -ne $subSection.PSObject.Properties['Level'])
@@ -47,18 +43,18 @@ function Out-JsonSection
                 {
                     [ref] $null = $sectionBuilder.Append((Out-JsonSection -Section $subSection))
                 }
-                'PScribo.Paragraph'
-                {
-                    [ref] $null = $sectionBuilder.Append((Out-JsonParagraph -Paragraph $subSection))
-                }
-                'PScribo.Table'
-                {
-                    [ref] $null = $sectionBuilder.Append((Out-JsonTable -Table $subSection))
-                }
-                'PScribo.Image'
-                {
-                    [ref] $null = $sectionBuilder.Append((Out-JsonImage -Image $subSection))
-                }
+                # 'PScribo.Paragraph'
+                # {
+                #     [ref] $null = $sectionBuilder.Append((Out-JsonParagraph -Paragraph $subSection))
+                # }
+                # 'PScribo.Table'
+                # {
+                #     [ref] $null = $sectionBuilder.Append((Out-JsonTable -Table $subSection))
+                # }
+                # 'PScribo.Image'
+                # {
+                #     [ref] $null = $sectionBuilder.Append((Out-JsonImage -Image $subSection))
+                # }
                 Default
                 {
                     Write-PScriboMessage -Message ($localized.PluginUnsupportedSection -f $subSection.Type) -IsWarning
@@ -66,8 +62,8 @@ function Out-JsonSection
             }
         }
 
-        [ref] $null = $sectionBuilder.AppendLine(']')
+        [ref] $null = $sectionBuilder.Add($sectionName, $section)
 
-        return $sectionBuilder.ToString()
+        return $sectionBuilder
     }
 }
