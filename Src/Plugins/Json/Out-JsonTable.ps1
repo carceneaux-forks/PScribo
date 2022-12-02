6cf237e5eebd33411712fd49354ac60abb9acb88
Chris Arceneaux <carcenea@gmail.com>
2022-12-02 17:04:52 -0500

    debugging
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTable.ps1
+++ Src/Plugins/Json/Out-JsonTable.ps1
@@ -30,44 +30,43 @@ function Out-JsonTable
                 $property.Value = Resolve-PScriboToken -InputObject $property.Value
             }
         }
-        Write-Host $Table
-        Write-Host $Table | ConvertTo-Json -Depth 100
-        # ## Rendering the table
-        # if ($Table.IsKeyedList)
-        # {
-        #     ## Create new objects with headings as properties
-        #     # $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
-        #     #                 Select-Object -Property * -ExcludeProperty '*__Style' |
-        #     #                 Format-Table -Wrap -AutoSize |
-        #     #                     Out-String).Trim([System.Environment]::NewLine)
-        #     $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
-        #                     Select-Object -Property * -ExcludeProperty '*__Style')
-        # }
-        # elseif ($Table.IsList)
-        # {
-        #     # $tableText = ($Table.Rows |
-        #     #     Select-Object -Property * -ExcludeProperty '*__Style' |
-        #     #         Format-List | Out-String).Trim([System.Environment]::NewLine)
-        #     $tableText = ($Table.Rows |
-        #         Select-Object -Property * -ExcludeProperty '*__Style')
-        # }
-        # else
-        # {
-        #     ## Don't trim tabs for table headers
-        #     ## Tables set to AutoSize as otherwise rendering is different between PoSh v4 and v5
-        #     # $tableText = ($Table.Rows |
-        #     #                 Select-Object -Property * -ExcludeProperty '*__Style' |
-        #     #                     Format-Table -Wrap -AutoSize |
-        #     #                         Out-String).Trim([System.Environment]::NewLine)
-        #     $tableText = ($Table.Rows |
-        #                     Select-Object -Property * -ExcludeProperty '*__Style')
-        # }
+        
+        ## Rendering the table
+        if ($Table.IsKeyedList)
+        {
+            ## Create new objects with headings as properties
+            # $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
+            #                 Select-Object -Property * -ExcludeProperty '*__Style' |
+            #                 Format-Table -Wrap -AutoSize |
+            #                     Out-String).Trim([System.Environment]::NewLine)
+            $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
+                            Select-Object -Property * -ExcludeProperty '*__Style')
+        }
+        elseif ($Table.IsList)
+        {
+            # $tableText = ($Table.Rows |
+            #     Select-Object -Property * -ExcludeProperty '*__Style' |
+            #         Format-List | Out-String).Trim([System.Environment]::NewLine)
+            $tableText = ($Table.Rows |
+                Select-Object -Property * -ExcludeProperty '*__Style')
+        }
+        else
+        {
+            ## Don't trim tabs for table headers
+            ## Tables set to AutoSize as otherwise rendering is different between PoSh v4 and v5
+            # $tableText = ($Table.Rows |
+            #                 Select-Object -Property * -ExcludeProperty '*__Style' |
+            #                     Format-Table -Wrap -AutoSize |
+            #                         Out-String).Trim([System.Environment]::NewLine)
+            $tableText = ($Table.Rows |
+                            Select-Object -Property * -ExcludeProperty '*__Style')
+        }
 
         # Write-Host "tableText type: $($tableText.GetType())"
-        # Write-Host "tableText unformatted:"
-        # Write-Host $tableText
-        # Write-Host "tableText json:"
-        # Write-Host $tableText | ConvertTo-Json -Depth 100
+        Write-Host "tableText unformatted:"
+        Write-Host $tableText
+        Write-Host "tableText json:"
+        Write-Host $tableText | ConvertTo-Json -Depth 100
         
         # [ref] $null = $tableBuilder.Append($tableJson)
         [ref] $null = $tableBuilder.Add("Table Name","Table Data")
