4df349326d8ab48b9f80ee5dd5683e01b5e1acd8
Chris Arceneaux <carcenea@gmail.com>
2022-12-05 09:52:01 -0500

    test
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTable.ps1
+++ Src/Plugins/Json/Out-JsonTable.ps1
@@ -21,6 +21,7 @@ function Out-JsonTable
         ## We need to replace page numbers before outputting the table
         foreach ($row in $Table.Rows)
         {
+            Write-Output $row.PSObject | ConvertTo-Json -Depth 100
             foreach ($property in $row.PSObject.Properties)
             {
                 if ($property.Value -is [System.Array])
@@ -31,48 +32,48 @@ function Out-JsonTable
             }
         }
         
-        ## Rendering the table
-        if ($Table.IsKeyedList)
-        {
-            Write-Host "Keyed List"
-            ## Create new objects with headings as properties
-            # $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
-            #                 Select-Object -Property * -ExcludeProperty '*__Style' |
-            #                 Format-Table -Wrap -AutoSize |
-            #                     Out-String).Trim([System.Environment]::NewLine)
-            $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
-                            Select-Object -Property * -ExcludeProperty '*__Style') #|
-                            #Format-Table -Wrap -AutoSize
-        }
-        elseif ($Table.IsList)
-        {
-            Write-Host "List"
-            # $tableText = ($Table.Rows |
-            #     Select-Object -Property * -ExcludeProperty '*__Style' |
-            #         Format-List | Out-String).Trim([System.Environment]::NewLine)
-            $tableText = ($Table.Rows |
-                Select-Object -Property * -ExcludeProperty '*__Style') #|
-                #Format-Table -Wrap -AutoSize
-        }
-        else
-        {
-            Write-Host "Not a List"
-            ## Don't trim tabs for table headers
-            ## Tables set to AutoSize as otherwise rendering is different between PoSh v4 and v5
-            # $tableText = ($Table.Rows |
-            #                 Select-Object -Property * -ExcludeProperty '*__Style' |
-            #                     Format-Table -Wrap -AutoSize |
-            #                         Out-String).Trim([System.Environment]::NewLine)
-            $tableText = ($Table.Rows |
-                            Select-Object -Property * -ExcludeProperty '*__Style') #|
-                            #Format-Table -Wrap -AutoSize
-        }
+        # ## Rendering the table
+        # if ($Table.IsKeyedList)
+        # {
+        #     Write-Host "Keyed List"
+        #     ## Create new objects with headings as properties
+        #     # $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
+        #     #                 Select-Object -Property * -ExcludeProperty '*__Style' |
+        #     #                 Format-Table -Wrap -AutoSize |
+        #     #                     Out-String).Trim([System.Environment]::NewLine)
+        #     $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
+        #                     Select-Object -Property * -ExcludeProperty '*__Style') #|
+        #                     #Format-Table -Wrap -AutoSize
+        # }
+        # elseif ($Table.IsList)
+        # {
+        #     Write-Host "List"
+        #     # $tableText = ($Table.Rows |
+        #     #     Select-Object -Property * -ExcludeProperty '*__Style' |
+        #     #         Format-List | Out-String).Trim([System.Environment]::NewLine)
+        #     $tableText = ($Table.Rows |
+        #         Select-Object -Property * -ExcludeProperty '*__Style') #|
+        #         #Format-Table -Wrap -AutoSize
+        # }
+        # else
+        # {
+        #     Write-Host "Not a List"
+        #     ## Don't trim tabs for table headers
+        #     ## Tables set to AutoSize as otherwise rendering is different between PoSh v4 and v5
+        #     # $tableText = ($Table.Rows |
+        #     #                 Select-Object -Property * -ExcludeProperty '*__Style' |
+        #     #                     Format-Table -Wrap -AutoSize |
+        #     #                         Out-String).Trim([System.Environment]::NewLine)
+        #     $tableText = ($Table.Rows |
+        #                     Select-Object -Property * -ExcludeProperty '*__Style') #|
+        #                     #Format-Table -Wrap -AutoSize
+        # }
 
-        # Write-Host "tableText type: $($tableText.GetType())"
-        Write-Host "tableText unformatted:"
-        Write-Host $tableText
-        Write-Host "tableText json:"
-        Write-Host $tableText | ConvertTo-Json -Depth 100
+        # # Write-Host "tableText type: $($tableText.GetType())"
+        # Write-Host "tableText unformatted:"
+        # Write-Host $tableText
+        # Write-Host "tableText json:"
+        # Write-Host $tableText | ConvertTo-Json -Depth 100
         
         # [ref] $null = $tableBuilder.Append($tableJson)
         [ref] $null = $tableBuilder.Add("Table Name","Table Data")
