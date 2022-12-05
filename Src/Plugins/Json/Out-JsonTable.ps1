65a4e9bc8766d0f87e43e2472e8468d0cc7c9c2d
Chris Arceneaux <carcenea@gmail.com>
2022-12-05 10:05:20 -0500

    test
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTable.ps1
+++ Src/Plugins/Json/Out-JsonTable.ps1
@@ -17,66 +17,25 @@ function Out-JsonTable
         $tableBuilder = [ordered]@{}
     }
     process
-    {
-        $number = 1
-        ## We need to replace page numbers before outputting the table
+    {        
         foreach ($row in $Table.Rows)
         {
-            Write-Host $number
-            $number++
-            Write-Host $row
-            foreach ($property in $row.PSObject.Properties)
-            {
-                if ($property.Value -is [System.Array])
-                {
-                    $property.Value = [System.String]::Join(' ', $property.Value)
-                }
-                $property.Value = Resolve-PScriboToken -InputObject $property.Value
-            }
+            Write-Host $row.Name
+            # foreach ($property in $row.PSObject.Properties)
+            # {
+            #     if (-not ($property.Name).EndsWith('__Style', 'CurrentCultureIgnoreCase'))
+            #     {
+            #         $propertyId = ($property.Name -replace '[^a-z0-9-_\.]','').ToLower()
+            #         $rowElement = $groupElement.AppendChild($xmlDocument.CreateElement($propertyId))
+            #         ## Only add the Name attribute if there's a difference
+            #         if ($property.Name -ne $propertyId)
+            #         {
+            #             [ref] $null = $rowElement.SetAttribute('name', $property.Name)
+            #         }
+            #         [ref] $null = $rowElement.AppendChild($xmlDocument.CreateTextNode($row.($property.Name)))
+            #     }
+            # }
         }
-        
-        # ## Rendering the table
-        # if ($Table.IsKeyedList)
-        # {
-        #     Write-Host "Keyed List"
-        #     ## Create new objects with headings as properties
-        #     # $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
-        #     #                 Select-Object -Property * -ExcludeProperty '*__Style' |
-        #     #                 Format-Table -Wrap -AutoSize |
-        #     #                     Out-String).Trim([System.Environment]::NewLine)
-        #     $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
-        #                     Select-Object -Property * -ExcludeProperty '*__Style') #|
-        #                     #Format-Table -Wrap -AutoSize
-        # }
-        # elseif ($Table.IsList)
-        # {
-        #     Write-Host "List"
-        #     # $tableText = ($Table.Rows |
-        #     #     Select-Object -Property * -ExcludeProperty '*__Style' |
-        #     #         Format-List | Out-String).Trim([System.Environment]::NewLine)
-        #     $tableText = ($Table.Rows |
-        #         Select-Object -Property * -ExcludeProperty '*__Style') #|
-        #         #Format-Table -Wrap -AutoSize
-        # }
-        # else
-        # {
-        #     Write-Host "Not a List"
-        #     ## Don't trim tabs for table headers
-        #     ## Tables set to AutoSize as otherwise rendering is different between PoSh v4 and v5
-        #     # $tableText = ($Table.Rows |
-        #     #                 Select-Object -Property * -ExcludeProperty '*__Style' |
-        #     #                     Format-Table -Wrap -AutoSize |
-        #     #                         Out-String).Trim([System.Environment]::NewLine)
-        #     $tableText = ($Table.Rows |
-        #                     Select-Object -Property * -ExcludeProperty '*__Style') #|
-        #                     #Format-Table -Wrap -AutoSize
-        # }
-
-        # # Write-Host "tableText type: $($tableText.GetType())"
-        # Write-Host "tableText unformatted:"
-        # Write-Host $tableText
-        # Write-Host "tableText json:"
-        # Write-Host $tableText | ConvertTo-Json -Depth 100
         
         # [ref] $null = $tableBuilder.Append($tableJson)
         [ref] $null = $tableBuilder.Add("Table Name","Table Data")
