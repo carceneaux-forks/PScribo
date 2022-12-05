e3417208b63840521ffc8d07f339f0072333fb59
Chris Arceneaux <carcenea@gmail.com>
2022-12-05 10:08:16 -0500

    test
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTable.ps1
+++ Src/Plugins/Json/Out-JsonTable.ps1
@@ -17,25 +17,61 @@ function Out-JsonTable
         $tableBuilder = [ordered]@{}
     }
     process
-    {        
+    {
         foreach ($row in $Table.Rows)
         {
-            Write-Host $row.Name
+            ## Rendering the table
+            if ($Table.IsKeyedList)
+            {
+                Write-Host "Keyed List"
+                Write-Host $row.Name
+                ## Create new objects with headings as properties
+                # $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
+                #                 Select-Object -Property * -ExcludeProperty '*__Style' |
+                #                 Format-Table -Wrap -AutoSize |
+                #                     Out-String).Trim([System.Environment]::NewLine)
+                $tableText = (ConvertTo-PSObjectKeyedListTable -Table $Table |
+                                Select-Object -Property * -ExcludeProperty '*__Style') #|
+                                #Format-Table -Wrap -AutoSize
+            }
+            elseif ($Table.IsList)
+            {
+                Write-Host "List"
+                Write-Host $row.Name
+                # $tableText = ($Table.Rows |
+                #     Select-Object -Property * -ExcludeProperty '*__Style' |
+                #         Format-List | Out-String).Trim([System.Environment]::NewLine)
+                $tableText = ($Table.Rows |
+                    Select-Object -Property * -ExcludeProperty '*__Style') #|
+                    #Format-Table -Wrap -AutoSize
+            }
+            else
+            {
+                Write-Host "Not a List"
+                Write-Host $row
+                ## Don't trim tabs for table headers
+                ## Tables set to AutoSize as otherwise rendering is different between PoSh v4 and v5
+                # $tableText = ($Table.Rows |
+                #                 Select-Object -Property * -ExcludeProperty '*__Style' |
+                #                     Format-Table -Wrap -AutoSize |
+                #                         Out-String).Trim([System.Environment]::NewLine)
+                $tableText = ($Table.Rows |
+                                Select-Object -Property * -ExcludeProperty '*__Style') #|
+                                #Format-Table -Wrap -AutoSize
+            }
+            
+            
             # foreach ($property in $row.PSObject.Properties)
             # {
-            #     if (-not ($property.Name).EndsWith('__Style', 'CurrentCultureIgnoreCase'))
+            #     if ($property.Value -is [System.Array])
             #     {
-            #         $propertyId = ($property.Name -replace '[^a-z0-9-_\.]','').ToLower()
-            #         $rowElement = $groupElement.AppendChild($xmlDocument.CreateElement($propertyId))
-            #         ## Only add the Name attribute if there's a difference
-            #         if ($property.Name -ne $propertyId)
-            #         {
-            #             [ref] $null = $rowElement.SetAttribute('name', $property.Name)
-            #         }
-            #         [ref] $null = $rowElement.AppendChild($xmlDocument.CreateTextNode($row.($property.Name)))
+            #         $property.Value = [System.String]::Join(' ', $property.Value)
             #     }
+            #     $property.Value = Resolve-PScriboToken -InputObject $property.Value
             # }
         }
+
+
         
         # [ref] $null = $tableBuilder.Append($tableJson)
         [ref] $null = $tableBuilder.Add("Table Name","Table Data")
