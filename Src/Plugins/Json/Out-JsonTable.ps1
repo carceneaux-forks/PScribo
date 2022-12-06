b3980b48bdfd2ba7bc2e41b4c5b39e954b134951
Chris Arceneaux <carcenea@gmail.com>
2022-12-06 14:26:58 -0500

    debugging
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTable.ps1
+++ Src/Plugins/Json/Out-JsonTable.ps1
@@ -27,6 +27,7 @@ function Out-JsonTable
 
         [ref] $null = $tableBuilder.Add(($Table.Rows | Select-Object -Property * -ExcludeProperty '*__Style'))
         Write-Host "tableBuilder: $($tableBuilder.Count)"
+        Write-Host "tableBuilder[1]: $($tableBuilder.Count)"
         Write-Host ":::: END ::::"
         return $tableBuilder
     }
