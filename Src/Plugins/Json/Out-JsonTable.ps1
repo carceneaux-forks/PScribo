f42088ee83bb13b88cc222a56c01aabb8fc043e0
Chris Arceneaux <carcenea@gmail.com>
2022-12-06 09:59:16 -0500

    debugging
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTable.ps1
+++ Src/Plugins/Json/Out-JsonTable.ps1
@@ -11,13 +11,20 @@ function Out-JsonTable
         [Parameter(Mandatory, ValueFromPipeline)]
         [System.Management.Automation.PSObject] $Table
     )
+    begin
+    {
+        ## Initializing table object
+        $tableBuilder = [System.Collections.ArrayList]::new()
+    }
     process
     {
         if ($Table.HasCaption)
         {
-            [ref] $null = $Table.Rows.Insert(0, (Get-JsonTableCaption -Table $Table))
+            [ref] $null = $tableBuilder.Add((Get-JsonTableCaption -Table $Table))
         }
+        
+        [ref] $null = $tableBuilder.Add(($Table.Rows | Select-Object -Property * -ExcludeProperty '*__Style'))
 
-        return ($Table.Rows | Select-Object -Property * -ExcludeProperty '*__Style')
+        return $tableBuilder
     }
 }
