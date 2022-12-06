67aea997a897ec6b63f44ef7ed7e594125a99a21
Chris Arceneaux <carcenea@gmail.com>
2022-12-06 14:22:18 -0500

    debugging
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTable.ps1
+++ Src/Plugins/Json/Out-JsonTable.ps1
@@ -19,7 +19,7 @@ function Out-JsonTable
     process
     {
         Write-Host ":::: BEGINNING ::::"
-        Write-Host $Table.Rows.GetType()
+        Write-Host $Table.Rows.Length
         # Write-Host "Table: $($Table.Rows.Length)"
         if ($Table.HasCaption)
         {
