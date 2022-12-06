dc7cde006f3dc8efb4de58ba3230a63f1e7ba59d
Chris Arceneaux <carcenea@gmail.com>
2022-12-06 14:43:18 -0500

    reverting debug changes
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Plugins/Json/Out-JsonTable.ps1
+++ Src/Plugins/Json/Out-JsonTable.ps1
@@ -18,17 +18,13 @@ function Out-JsonTable
     }
     process
     {
-        Write-Host ":::: BEGINNING ::::"
-        Write-Host "Table: $($Table.Rows.Count)"
         if ($Table.HasCaption)
         {
             [ref] $null = $tableBuilder.Add((Get-JsonTableCaption -Table $Table))
         }
 
         [ref] $null = $tableBuilder.Add(($Table.Rows | Select-Object -Property * -ExcludeProperty '*__Style'))
-        Write-Host "tableBuilder: $($tableBuilder[0].Count)"
-        Write-Host "tableBuilder[1]: $($tableBuilder[1].Count)"
-        Write-Host ":::: END ::::"
+        
         return $tableBuilder
     }
 }
